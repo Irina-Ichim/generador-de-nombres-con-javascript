10f1ec330646aff3ba105dc5978ee02c103e9e58
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-03-30 23:00:11 +0200

    agregando bootstrap

--- js.js
+++ js.js
@@ -7,3 +7,16 @@ console.log('funciona');
  //   variable = Math.floor(Math.random() * 16);
  //   document.getElementById('random').innerHTML = variable;
 //}
+//let containeruno = document.querySelector('.containeruno');
+//let btn = document.getElementById('spin');
+//let number = Math.ceil(Math.random() * 1000);
+
+//btn.onclick = function() {
+   // containeruno.style.transform = "rotate(" + number + "deg)";
+    //number += Math.ceil(Math.random() * 1000);
+//}
+
+
+
+
+
