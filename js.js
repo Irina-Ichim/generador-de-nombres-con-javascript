a897f22e622a8191007fca3160ea78f6de6d02f9
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-08-04 21:54:55 +0200

    generador de nombres automatico

--- js.js
+++ js.js
@@ -1,66 +1,19 @@
 
-
-
-//function azar () {
-//   variable = Math.floor(Math.random() * 16);
-//   document.getElementById('random').innerHTML = variable;
-//}
-//let containeruno = document.querySelector('.containeruno');
-//let btn = document.getElementById('spin');
-//let number = Math.ceil(Math.random() * 1000);
-
-//btn.onclick = function() {
-// containeruno.style.transform = "rotate(" + number + "deg)";
-//number += Math.ceil(Math.random() * 1000);
-//}
-//variable = Math.floor(Math.random() * 16);
-//   document.getElementById('random').innerHTML = variable;
-//const numeros = [1, 2, 2, 3, 4, 4, 5];
-
-//const eliminaDuplicados = (arr) => {
-
-//  const unicos = [];
-//  arr.forEach( (elemento) => {
-//    if (!unicos.includes(elemento)) {
-//      unicos.push(elemento);
-//    }
-//  });
-
-// return unicos;
-//}
-
-//console.log(eliminaDuplicados(numeros)); // [1, 2, 3, 4, 5]
-
-
 let names = ["Ikram", "Irina", "Maribel", "Lola", "Javi", "Gabriela", "Pamela", "Manuel", "Minerva", "Nelly", "Jesabel", "Cyntia", "Jhoana", "Irina S.", "Tati", "Nadia", "Halima", "Telma"];
 
 document.getElementById("boton-elegir").addEventListener("click", generate);
 
-let emptyArrayNames = [] ;
-
 function generate() {
+    if (names.length === 0) {
+        alert('Game Over');
+        return;
+    }
   
-  if (names.length === 0){
-   
-    alert('Game Over')
-  }
+    let posicionAleatoria = Math.floor(Math.random() * names.length);
+    let personaElegida = names[posicionAleatoria];
   
-  let posicionAleatoria = Math.round(Math.random() * (names.length -1));
-  
-  let personaElegida = names[posicionAleatoria];
-  console.log(personaElegida);
-  names.splice(posicionAleatoria, 1) 
-  //pintarlo por pantalla
-  document.getElementById('result').innerHTML = personaElegida;
-  
-  // borrarlo del array
- 
-
- 
+    names.splice(posicionAleatoria, 1);
+    document.getElementById('result').innerHTML = personaElegida;
 }
 
- 
-
-
-
-
+console.log('irina')
